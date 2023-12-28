8a870722d6dcda3e91c6b12928588015cc2b1530
Ashish Mishra <ashish.mishra.sse@gmail.com>
2023-12-28 09:59:30 +0530

    Constructors changes

--- src/Session19/A.java
+++ src/Session19/A.java
@@ -2,8 +2,14 @@ package Session19;
 
 public class A
 {
-    public  A()
+    public A()
     {
-
+        System.out.println("Default Constructor");
+    }
+    String webDriver;
+    public  A(String webDriver)
+    {
+        System.out.println(this.webDriver);
+       this.webDriver=webDriver;
     }
 }
---
+++ src/Session19/B.java
@@ -0,0 +1,11 @@
+package Session19;
+
+public class B extends  A
+{
+    public static void main(String[] args) {
+        A a= new A("ChromeDriver");
+        System.out.println(a.webDriver);
+
+    }
+
+}
